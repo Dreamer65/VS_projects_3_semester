095e50b6e74841978935f9433524c3a45c179759
Sutormin Sergey <sergeysutormen11@live.ru>
2018-10-09 13:36:08 +1100

    Dont remember what cheged)))

--- lab_5/Program.cs
+++ lab_5/Program.cs
@@ -19,11 +19,15 @@ namespace lab_5
                 return;
             }
             Console.WriteLine("Способов поднятся на эту ступеньку у зайца:");
-            Console.WriteLine(Leders(N)+"\nНажмите любую клавишу...");
-            Console.ReadKey();
+            Console.WriteLine(Leders(N)); // Вывод колличества
+            
+            if (args.Length == 0) {
+                Console.WriteLine("\nНажмите любую клавишу..."); 
+                Console.ReadKey();
+            }
         }
 
-        static int Leders(int N)
+        static int Leders_old(int N)
         {
             switch (N) {
                 case 1:
@@ -33,9 +37,32 @@ namespace lab_5
                 case 3:
                     return 4;
                 default:
-                    return Leders(N - 1) + Leders(N - 2) + Leders(N - 3);
+                    return Leders_old(N - 1) + Leders_old(N - 2) + Leders_old(N - 3);
             }
 
+        }
+
+        static long Leders(int N)
+        {
+            if (N <= 0) return 0;
+            long sum = 0, n1 = 1, n2 = 2, n3 = 4;
+            if (N <= 3)
+            {
+                switch (N)
+                {
+                    case 1: return n1;
+                    case 2: return n2;
+                    case 3: return n3;
+                }
+            }
+            for (int i = 4; i <= N; i++)
+            {
+                sum = n1 + n2 + n3;
+                n1 = n2;
+                n2 = n3;
+                n3 = sum;
+            }
+            return sum;
         }
     }
 }
